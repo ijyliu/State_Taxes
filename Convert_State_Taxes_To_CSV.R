b7d75bafffc1b8d89ba33cfe30f51880f80a0126
Isaac Liu <67396209+ijyliu@users.noreply.github.com>
2023-04-14 20:04:48 -0500

    Add files via upload

--- Convert_State_Taxes_To_CSV.R
+++ Convert_State_Taxes_To_CSV.R
@@ -1,9 +1,13 @@
 
 # R packages to convert Stata dta file to csv format
-library('haven', 'readr')
+library('haven', 'readr', 'tidyverse')
 
 # Read in data
 tax_data <- read_dta('State_Taxes.dta')
+print(tax_data)
+
+# Reorder variables
+tax_data <- dplyr::transmute(tax_data, year, State, Corp_Rate, Corp_Rate_Adj, Pers_Rate, Pers_Rate_Adj, Sales_Rate, Sales_Rate_Adj)
 print(tax_data)
 
 # Write data
